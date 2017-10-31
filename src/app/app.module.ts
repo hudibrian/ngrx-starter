b2df4e20bdd245c2687fbd5294847ce6a71f8e19
Brian Hudi <Brian Hudi>
2017-10-30 23:20:55 -0500

    updated prettier to use single quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from "./app.component";
-import { AngularFireModule } from "angularfire2";
-import { AngularFireDatabaseModule } from "angularfire2/database";
-import { environment } from "../environments/environment";
-import { PostListComponent } from "./post-list/post-list.component";
-import { PostContainerComponent } from "./containers/postContainer/postContainer.component";
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostContainerComponent } from './containers/postContainer/postContainer.component';
 import {
   MatToolbarModule,
   MatCardModule,
   MatGridListModule
-} from "@angular/material";
-import { FlexLayoutModule } from "@angular/flex-layout";
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
