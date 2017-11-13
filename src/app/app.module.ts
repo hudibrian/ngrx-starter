a7b33c84700c0cc5c73e7b9b0bbcdcd800d251c8
Brian Hudi <bhudi@rightpoint.com>
2017-11-13 14:56:40 -0600

    set up router-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { routes } from './app.routes';
+import { StoreModule } from '@ngrx/store';
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { metaReducer } from './store/store';
+import { PostItemActionsEffects } from './store/effects/post-item-action.effects';
 import { environment } from '../environments/environment';
-import { PostListComponent } from './post-list/post-list.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { AppComponent } from './app.component';
 import { PostContainerComponent } from './containers/postContainer/postContainer.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostItemComponent } from './post-item/post-item.component';
+import { ContentLayoutComponent } from './content-layout-component/content-layout.component';
+import { UserDetailContainerComponent } from './containers/userDetailContainer/user-detail-container.component';
+import { UserDetailItemComponent } from './user-detail-item/user-detail-item.component';
+import { ItemDetailsContainerComponent } from './containers/commentsContainer/item-details-container.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { ItemDetailsCommentsComponent } from './item-details/item-details-comments/item-details-comments.component';
+import { PostItemActionsComponent } from './post-item-actions/post-item-actions.component';
+import { PostItemActionsContainerComponent } from './containers/postItemActionContainer/post-item-action.container';
+import { HackerNewsServiceService } from './services/hacker-news/hacker-news.service';
 import {
   MatToolbarModule,
   MatCardModule,
   MatGridListModule,
-  MatChipsModule,
   MatButtonModule,
   MatSnackBarModule
 } from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HackerNewsServiceService } from './services/hacker-news/hacker-news.service';
-import { Http, HttpModule } from '@angular/http';
-import { PostItemComponent } from './post-item/post-item.component';
-import { RouterModule } from '@angular/router';
-import { routes } from './app.routes';
-import { ContentLayoutComponent } from './content-layout-component/content-layout.component';
-import { UserDetailContainerComponent } from './containers/userDetailContainer/user-detail-container.component';
-import { UserDetailItemComponent } from './user-detail-item/user-detail-item.component';
-import { ItemDetailsComponent } from './item-details/item-details.component';
-import { ItemDetailsContainerComponent } from './containers/commentsContainer/item-details-container.component';
-import { ItemDetailsCommentsComponent } from './item-details/item-details-comments/item-details-comments.component';
-import { metaReducer } from './store/store';
-import { StoreModule } from '@ngrx/store';
-import { PostItemActionsComponent } from './post-item-actions/post-item-actions.component';
-import { PostItemActionsContainerComponent } from './containers/postItemActionContainer/post-item-action.container';
-import { EffectsModule } from '@ngrx/effects';
-import { PostItemActionsEffects } from './store/effects/post-item-action.effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -49,8 +47,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FlexLayoutModule,
     MatSnackBarModule,
     RouterModule.forRoot(routes),
-    StoreModule.forRoot({ reducer: metaReducer }),
+    StoreModule.forRoot({ routerReducer: routerReducer, reducer: metaReducer }),
     EffectsModule.forRoot([PostItemActionsEffects]),
+    StoreRouterConnectingModule,
     !environment.production
       ? StoreDevtoolsModule.instrument({ maxAge: 50 })
       : []
